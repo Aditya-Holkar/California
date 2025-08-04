097fdedff2f93db61b90de953f24575963c01d0c
Aditya-Holkar <adiholkar555@gmail.com>
2025-08-04 10:35:51 +0530

    first commit

--- app/(main)/Management/page.tsx
+++ app/(main)/Management/page.tsx
@@ -1,7 +1,14 @@
 // app/manage/page.tsx
 "use client";
 
-import Manage from "../../Components/Manage";
+// import Manage from "../../Components/Manage";
+import dynamic from "next/dynamic";
+
+// Dynamically import the component that uses `window` with SSR disabled
+const Manage = dynamic(
+  () => import("../../Components/Manage"), // Replace with your component
+  { ssr: false }
+);
 
 export default function ManagePage() {
   return (
